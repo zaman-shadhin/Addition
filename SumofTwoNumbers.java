5d008de6d4fb63af9557073fa088f88acf3b9b7b
zaman-shadhin <zamanshadhin26@gmail.com>
2025-03-02 10:29:42 +0600

    Add files via upload

---
+++ SumofTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumofTwoNumbers {
+
+
+
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter two number : ");
+         int a = scanner.nextInt();
+         int b = scanner.nextInt();
+
+      
+
+     int sum = a+b;
+        
+    System.out.println("Sum of two number : "+sum);
+    }
+}
